a44bb39b4882bc7754574bc80131c463d388588c
NikolayEmrich <emrich.work@gmail.com>
2024-11-13 09:50:06 +0100

    lesson10 started

--- src/components/myButton/myButton.tsx
+++ src/components/myButton/myButton.tsx
@@ -14,7 +14,8 @@
 
 //  ------ ВАРИАНТ КОДА С ДЕСТРУКТУРИЗАЦИЕЙ ------
 // Забираем значения из объекта по ключам и кладем их в переменные с определенным именем в одну строчку
-import './myButton.css'
+import styles from './myButton.module.css'
+import cn from 'classnames'
 
 interface IMyButtonProps {
      text?: string,
@@ -33,7 +34,14 @@ function MyButton({func = handleDefaultClick, isDanger = true, text = 'Click', m
      return <button 
           type = {myType}
           onClick = {func} 
-          className = {`my-button ${isDanger ? 'btn-danger' : 'btn-primary'}`}>
+          className = {cn(styles.myButton, {
+               // Если isDanger будет true - Добавляем класс, если нет - то не добавляем.
+               [styles.btnDanger]:isDanger,
+               [styles.btnPrimary]:!isDanger
+
+          })}>
+               {/* Удаляем предыдующую версию записи классов, написанную без classNames */}
+          {/* className = {`my-button ${isDanger ? 'btn-danger' : 'btn-primary'}`} */}
           {text}
      </button>;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import FetchDog from './components/fetchDog/FetchDog';
-// import Lesson06 from './lessons/lesson06/Lesson06';
-// import Lesson07 from './lessons/lesson07/Lesson07';
-// import Lesson04 from './lessons/lesson04/Lesson04';
-// import Lesson05 from './lessons/lesson05/Lesson05';
-// import Lesson08 from './lessons/lesson08/Lesson08';
+import Lesson09 from './lessons/lesson09/Lesson09';
 
 
 const root = ReactDOM.createRoot(
@@ -18,7 +13,11 @@ root.render(
     {/* <Lesson07/> */}
     {/* <Lesson05/> */}
     {/* <Lesson08/> */}
-    <FetchDog/>
+    {/* <FetchDog/> */}
+    {/* <FetchFox/> */}
+    <Lesson09/>
+
+
   </>
 );
 
